f0de60509e1f5f8b88f9f4edd9e8d4109a3f69e4
abdalladaud <abdullahidaud69@gmail.com>
2026-09-22 11:27:15 +0300

    Configure production API URL

--- frontend/src/lib/api/apiclient.js
+++ frontend/src/lib/api/apiclient.js
@@ -1,24 +1,24 @@
-import axios from "axios";
-import useAuthStore from "../store/authStore";
+import axios from "axios"
+import useAuthStore from "../store/authStore"
 
-const API_URL = "/api";
+const API_URL =
+  import.meta.env.VITE_API_URL || "/api"
 
 const api = axios.create({
   baseURL: API_URL,
-});
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
 
 api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
+  const token = useAuthStore.getState().token
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
 
-  if (!(config.data instanceof FormData)) {
-    config.headers["Content-Type"] = "application/json";
-  }
+  return config
+})
 
-  return config;
-});
-
-export default api;
+export default api
